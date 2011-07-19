186baf82d885130cdb410e293d8d4eebe6c4303f
Kim Lima <kimlima9@gmail.com>
2011-07-19 11:10:35 -0300

    Adicionado o carrinho de compras, e botões de comprar na interface

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,13 @@
 class StoreController < ApplicationController
   def index
     @products = Product.all
+    
+    if session[:counter].nil?
+      session[:counter] = 0
+    end
+    
+    session[:counter] += 1
+    
   end
 
 end
