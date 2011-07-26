342477c190dbda79474a0fb2cda07ee1da86a996
Kim Lima <kimlima9@gmail.com>
2011-07-26 19:05:43 -0300

    Agrupa items e mostra preço, além de salvar o preço atual no item de linha

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,14 +1,18 @@
 class Cart < ActiveRecord::Base
   has_many :line_items, :dependent => :destroy
   
-  def add_product(product_id)
+  def add_product(product_id, product_price)
     current_item = line_items.where(:product_id => product_id).first
     if current_item
-      current_item += 1
+      current_item.quantity += 1
     else
-      current_item = LineItem.new(:product_id => product_id)
+      current_item = LineItem.new(:product_id => product_id, :price => product_price)
       line_items << current_item
     end
     current_item
+  end
+  
+  def total_price
+    line_items.to_a.sum { |item| item.total_price }
   end
 end
--- test/unit/cart_test.rb
+++ test/unit/cart_test.rb
@@ -1,8 +1,19 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  def new_cart_with_one_product(product_name)
+    cart = Cart.new
+    cart.add_product(products(product_name).id)
+    cart
   end
+  
+  test 'cart should create a new line item when adding a new product' do
+    cart = new_cart_with_one_product(:one)
+    assert_equal 1, cart.line_items.count
+    # Add a new product
+    cart.add_product(products(:ruby).id)
+    assert_equal 2, cart.line_items.count
+  end
+
 end
